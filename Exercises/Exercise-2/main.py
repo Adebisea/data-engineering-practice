5ea258fd7ba2f50ce6d3e06c7e1d42837c154df6
Adebisea <abdulgkhadijah@gmail.com>
2024-06-19 14:39:02 +0100

    implemented a function to scrap the data

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,29 @@
 import requests
-import pandas
+from bs4 import BeautifulSoup
+import pandas as pd
 
 
-def main():
-    # your code here
-    pass
+url = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+
+
+def scrape_data(url):
+    data = requests.get(url)
+    soup = BeautifulSoup(data.content, 'html.parser')
+    d_table = soup.find_all('tr')
+
+    dict_table = {row.contents[1].text:row.contents[0].text  for row in d_table[3:-1]}
+
+
+    csv_file_link = dict_table.get('2024-01-19 10:31  ')
+    return csv_file_link
+
+
+
+def main(url):
+    scrape_data(url)
 
 
 if __name__ == "__main__":
-    main()
+    main(url)
+
+
